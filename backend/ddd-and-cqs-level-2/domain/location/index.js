a5bf20724db4a51298460b6137fee75fdc7d956f
adebray <debray.arnaud@gmail.com>
2021-09-19 13:16:56 +0200

    Adding altitude to a location for the localize-vehicle cli command

--- backend/ddd-and-cqs-level-2/domain/location/index.js
+++ backend/ddd-and-cqs-level-2/domain/location/index.js
@@ -2,7 +2,8 @@ const { ValueObject, deepEquals } = require('../lib')
 
 const LocationData = {
     latitude: Number,
-    longitude: Number
+    longitude: Number,
+    altitude: Number
 }
 
 exports.Location = class extends ValueObject {
@@ -10,10 +11,11 @@ exports.Location = class extends ValueObject {
 
     get latitude() { return this._data.latitude }
     get longitude() { return this._data.longitude }
+    get altitude() { return this._data.altitude }
 
     equals(o) {
         return o && this.latitude == o.latitude
             && this.longitude == o.longitude
+            && this.altitude == o.altitude
     }
 }
-
